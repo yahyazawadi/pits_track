3c5e92541ca9a8770e36ebc2fa6c08ab0d3c0d89
Yahyazawadi <yahyazawadi278@gmail.com>
2025-10-14 20:43:01 +0300

    added bottomnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_taking_app/data%20manager/notes_manager.dart';
+import 'package:note_taking_app/models/note.dart';
+import 'package:note_taking_app/models/task.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +15,34 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+  static DataManager<Note> notesManager = DataManager(
+    filename: "notes.json",
+    fromJson: Note.fromJson,
+  );
+  static DataManager<Task> tasksManager = DataManager(
+    filename: "tasks.json",
+    fromJson: Task.fromJson,
+  );
+  @override
+  void initState() {
+    super.initState();
+
+    notesManager.init();
+    tasksManager.init();
+  }
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,10 +54,23 @@ class MyAppState extends State<MyApp> {
           ),
         ),
         body: Container(
-          color: Colors.black,
+          color: Colors.grey[900],
           child: Center(
             child: Text('Hello, world!', style: TextStyle(color: Colors.white)),
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+
+          onTap: _onItemTapped,
+          currentIndex: _selectedIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.article), label: 'Notes'),
+
+            BottomNavigationBarItem(icon: Icon(Icons.task), label: 'Tasks'),
+          ],
         ),
       ),
     );
